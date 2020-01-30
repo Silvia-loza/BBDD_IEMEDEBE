989bf7ba8093243b0d12a17eb25cf8f6102ee677
Silvia Lozano <sylvilozano@gmail.com>
2020-01-30 12:35:16 +0100

    subiendo version 1

---
+++ main.ts
@@ -0,0 +1,46 @@
+import {Profesional} from "./profesional"
+import {Pelicula} from "./pelicula"
+
+let profe1: Profesional = new Profesional("Johnny Depp", 57, "hombre", 75, 178, "castaño", "marrón", "gitanito", false, "americano", 0, "actor");
+let profe2: Profesional = new Profesional("Pedro Almodóvar", 70, "hombre", 85, 168, "blanco", "negro", "blanquito", false, "español", 2, "director");
+let profe3: Profesional = new Profesional("Nicole Kidman", 52, "mujer", 65, 180, "pelirrojo", "azul", "blanquita", false, "australiana", 1, "actriz");
+let profe4: Profesional = new Profesional("Peter Jackson", 58, "hombre", 90, 169, "gris", "marrón", "blanco", false, "neozelandés", 3, "director");
+let profe5: Profesional = new Profesional("Will Smith", 51, "hombre", 75, 188, "negro", "negro", "negro", false, "americano", 0, "actor");
+let profe6: Profesional = new Profesional("Angelina Jolie", 44, "mujer", 65, 169, "moreno", "verdes", "blanca", false, "americano", 0, "actriz");
+let profe7: Profesional = new Profesional("Emma Watson", 29, "mujer", 58, 165, "rubio", "marrón", "blanquita", false, "británica", 0, "actriz");
+let profe8: Profesional = new Profesional("Stanley Kubrick", 70, "hombre", 75, 170, "negro", "marrón", "morenito", true, "americano", 1, "director");
+let profe9: Profesional = new Profesional("Emmanuel Lubezki", 55, "hombre", 70, 172, "calvo", "negros", "morenito", false, "mexicano", 3, "fotografía");
+let profe10: Profesional = new Profesional("John Williams", 87, "hombre", 85, 182, "blanco", "marrón", "blanco", true, "americano", 5, "compositor");
+
+let actores1: Profesional[] = [profe1, profe3, profe5];
+let actores2: Profesional[] = [profe5, profe7, profe1];
+let actores3: Profesional[] = [profe7, profe3, profe6];
+
+
+
+
+let peli1: Pelicula = new Pelicula("Titanic", 1997, "estadounidense", actores1, "James Cameron", "James Cameron", "inglés", "cine", false, "Jack y Rose", "Universal", "Universal");
+let peli2: Pelicula = new Pelicula("El Padrino", 1972, "estadounidense", actores2, "Francis Ford Coppola", "Mario Puzo", "italiano", "cine", false, "Vitto Corleone", "Warner", "Warner");
+let peli3: Pelicula = new Pelicula("El Bar", 2017, "española", actores3, "Alex de la Iglesia", "Antonio Pérez", "español", "televisión", false, "Manola", "Atresmedia", "Atresmedia");
+let peli4: Pelicula = new Pelicula("Toy Story", 1995, "estadounidense", actores1, "John Lasseter", "Joel Cohen", "inglés", "cine", false, "Woody y Buzz", "Pixar", "Disney");
+let peli5: Pelicula = new Pelicula("Los lunes al sol", 2002, "española", actores2, "Fernando León", "Ignacio del Moral", "español", "cine", false, "José Suárez", "Paramount", "Paramount");
+let peli6: Pelicula = new Pelicula("Bienvenidos al Norte", 2008, "francesa", actores3, "Dani Boon", "Antonio Pérez", "frnacés", "cine", false, "Philippe", "Pathé", "Pathé");
+let peli7: Pelicula = new Pelicula("La naranja mecánica", 1971, "británica", actores1, "Stanley Kubrick", "Stanley Kubrick", "inglés", "cine", false, "Alex", "Warner", "Warner");
+let peli8: Pelicula = new Pelicula("Up", 2009, "estadounidense", actores2, "Pete Docter", "Bob Peterson", "inglés", "cine", false, "Carl", "Disney", "Disney");
+let peli9: Pelicula = new Pelicula("La Ola", 2008, "alemana", actores3, "Dennis Gansel", "Dennis Gansel", "alemán", "internet", false, "Rainer", "Rat Pack", "Constantin Film");
+let peli10: Pelicula = new Pelicula("Amélie", 2001, "francesa", actores1, "Jean-Pierre Jeunet", "Guillaume Laurant", "francés", "cine", false, "Amélie", "Union", "Fox");
+
+
+
+profe1.mostrarValor();
+profe2.mostrarValor();
+profe3.mostrarValor();
+profe4.mostrarValor();
+profe5.mostrarValor();
+profe6.mostrarValor();
+profe7.mostrarValor();
+profe8.mostrarValor();
+profe9.mostrarValor();
+profe10.mostrarValor();
+
+
---
+++ profesional.ts
@@ -0,0 +1,44 @@
+export class Profesional {
+    nombre: string;
+    edad: number;
+    sexo: string;
+    peso: number;
+    altura: number;
+    colorPelo: string;
+    colorOjos: string;
+    raza: string;
+    estaRetirado: boolean;
+    nacionalidad: string;
+    numeroDeOscars: number;
+    profesion: string;
+
+    constructor(nombre: string, edad: number, sexo: string, peso: number, altura: number, colorPelo: string, colorOjos: string, raza: string, estaRetirado: boolean, nacionalidad: string, numeroDeOscars: number, profesion: string){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+        this.colorPelo = colorPelo;
+        this.colorOjos = colorOjos;
+        this.raza = raza;
+        this.estaRetirado = estaRetirado;
+        this.nacionalidad = nacionalidad;
+        this.numeroDeOscars = numeroDeOscars;
+        this.profesion = profesion;
+    }
+
+    
+    mostrarValor(){
+        let conjunto: string = "";
+        conjunto = "Las características de " + this.nombre + " son:" + "\n" + "      * Nombre: " + this.nombre + "\n" + "      * Edad: " +
+         this.edad + "\n" + "      * Sexo: " + this.sexo + "\n" + "      * Peso: " + this.peso + "\n" + "      * Altura: " + this.altura + 
+         "\n" + "      * Color de pelo: " + this.colorPelo + "\n" + "      * Color de ojos: " + this.colorOjos + "\n" + "      * Raza: " + this.raza + 
+         "\n" + "      * ¿Está retirado?: " + this.estaRetirado + "\n" + "      * Nacionalidad: " + this.nacionalidad + "\n" +
+         "      * Número de Oscars: " + this.numeroDeOscars + "\n" + "      * Profesión: " + this.profesion + "\n";
+
+         console.log(conjunto);
+    }
+
+
+
+}
