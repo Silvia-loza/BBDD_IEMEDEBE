6f68c5fbba247e8cfae97b98af36a408c0e83415
Irene Hita <irene.hita7@gmail.com>
2020-01-31 11:56:07 +0100

    subiendo cambios irene

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
+
 import {Profesional} from "./profesional"
 import {Pelicula} from "./pelicula"
+import {Iemedebe} from "./iemedebe"
 
 let profe1: Profesional = new Profesional("Johnny Depp", 57, "hombre", 75, 178, "castaño", "marrón", "gitanito", false, "americano", 0, "actor");
 let profe2: Profesional = new Profesional("Pedro Almodóvar", 70, "hombre", 85, 168, "blanco", "negro", "blanquito", false, "español", 2, "director");
@@ -18,20 +20,54 @@ let actores3: Profesional[] = [profe7, profe3, profe6];
 
 
 
-
-let peli1: Pelicula = new Pelicula("Titanic", 1997, "estadounidense", actores1, "James Cameron", "James Cameron", "inglés", "cine", false, "Jack y Rose", "Universal", "Universal");
-let peli2: Pelicula = new Pelicula("El Padrino", 1972, "estadounidense", actores2, "Francis Ford Coppola", "Mario Puzo", "italiano", "cine", false, "Vitto Corleone", "Warner", "Warner");
-let peli3: Pelicula = new Pelicula("El Bar", 2017, "española", actores3, "Alex de la Iglesia", "Antonio Pérez", "español", "televisión", false, "Manola", "Atresmedia", "Atresmedia");
-let peli4: Pelicula = new Pelicula("Toy Story", 1995, "estadounidense", actores1, "John Lasseter", "Joel Cohen", "inglés", "cine", false, "Woody y Buzz", "Pixar", "Disney");
-let peli5: Pelicula = new Pelicula("Los lunes al sol", 2002, "española", actores2, "Fernando León", "Ignacio del Moral", "español", "cine", false, "José Suárez", "Paramount", "Paramount");
-let peli6: Pelicula = new Pelicula("Bienvenidos al Norte", 2008, "francesa", actores3, "Dani Boon", "Antonio Pérez", "frnacés", "cine", false, "Philippe", "Pathé", "Pathé");
-let peli7: Pelicula = new Pelicula("La naranja mecánica", 1971, "británica", actores1, "Stanley Kubrick", "Stanley Kubrick", "inglés", "cine", false, "Alex", "Warner", "Warner");
-let peli8: Pelicula = new Pelicula("Up", 2009, "estadounidense", actores2, "Pete Docter", "Bob Peterson", "inglés", "cine", false, "Carl", "Disney", "Disney");
-let peli9: Pelicula = new Pelicula("La Ola", 2008, "alemana", actores3, "Dennis Gansel", "Dennis Gansel", "alemán", "internet", false, "Rainer", "Rat Pack", "Constantin Film");
-let peli10: Pelicula = new Pelicula("Amélie", 2001, "francesa", actores1, "Jean-Pierre Jeunet", "Guillaume Laurant", "francés", "cine", false, "Amélie", "Union", "Fox");
-
+let peli1: Pelicula = new Pelicula("Titanic", 1997, "estadounidense", "drama");
+let peli2: Pelicula = new Pelicula("El Padrino", 1972, "estadounidense", "drama");
+let peli3: Pelicula = new Pelicula("El Bar", 2017, "española", "comedia");
+let peli4: Pelicula = new Pelicula("Toy Story", 1995, "estadounidense", "comedia");
+let peli5: Pelicula = new Pelicula("Los lunes al sol", 2002, "española",);
+let peli6: Pelicula = new Pelicula("Bienvenidos al Norte", 2008, "francesa",);
+let peli7: Pelicula = new Pelicula("La naranja mecánica", 1971, "británica",);
+let peli8: Pelicula = new Pelicula("Up", 2009, "estadounidense",);
+let peli9: Pelicula = new Pelicula("La Ola", 2008, "alemana",);
+let peli10: Pelicula = new Pelicula("Amélie", 2001, "francesa",);
 
 
+
+peli8.actores = actores2;
+peli8.director = "Pete Docter";
+peli8.guionista = "Bob Peterson";
+peli8.idioma = "inglés";
+peli8.plataforma = "cine";
+peli8.esMCU = false;
+peli8.nombreProtagonista = "Carl";
+peli8.productora = "Disney";
+peli8.distribuidora = "Disney";
+
+peli9.actores = actores3;
+peli9.director = "Dennis Gansel";
+peli9.guionista = "Dennis Gansel";
+peli9.idioma = "alemán";
+peli9.plataforma = "internet";
+peli9.esMCU = false;
+peli9.nombreProtagonista = "Rainer";
+peli9.productora = "Rat Pack";
+peli9.distribuidora = "Constantin Film";
+
+peli10.actores = actores1;
+peli10.director = "Jean-Pierre Jeunet";
+peli10.guionista = "Guillaume Laurant";
+peli10.idioma = "francés";
+peli10.plataforma = "cine";
+peli10.esMCU = false;
+peli10.nombreProtagonista = "Amélie";
+peli10.productora = "Union";
+peli10.distribuidora = "Fox";
+    
+
+
+let ieme1: Iemedebe = new Iemedebe([peli3, peli5, peli7, peli10]);
+
+/*
 profe1.mostrarValor();
 profe2.mostrarValor();
 profe3.mostrarValor();
@@ -41,6 +77,28 @@ profe6.mostrarValor();
 profe7.mostrarValor();
 profe8.mostrarValor();
 profe9.mostrarValor();
-profe10.mostrarValor();
+profe10.mostrarValor();*/
 
+
+/*peli1.mostrarDatos();
+peli2.mostrarDatos();
+peli3.mostrarDatos();
+peli4.mostrarDatos();
+peli5.mostrarDatos();
+peli6.mostrarDatos();
+peli7.mostrarDatos();
+peli8.mostrarDatos();
+peli9.mostrarDatos();
+peli10.mostrarDatos();*/
+
+
+//console.log(ieme1)
+
+var myJson = JSON.stringify(ieme1)
+
+var miVariable = require('fs');
+miVariable.writeFile("imdbBBDD.json", myJson, function(err, result) {
+    if(err) {console.log('error', err)}
+    else{console.log("Tu archivo se ha generado correctamente")};
+})
 
